3de8b8946b5b338303a6f949742c8dbe99161109
redkin <ilya.redking@gmail.com>
2020-08-28 16:58:00 -0400

    step 3 - done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
+
+// const readlineSync = require('readline-sync');
 console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+
+
