b0fc52c249e5212e1d8caf10663344b8e8f9c4d9
redkin <ilya.redking@gmail.com>
2020-08-31 13:43:17 -0400

    cli.js added

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
+import askName from '../src/cli.js';
 
-import readlineSync from 'readline-sync';
-
-// const readlineSync = require('readline-sync');
 console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
+askName();
 
 
---
+++ src/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => {
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+}
+
+export default askName;
+
+
+
+
