08197ed1ceda8957b33c5abf01b55d78babc4e21
redkin <ilya.redking@gmail.com>
2020-08-31 16:13:45 -0400

    step 4, part 1

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,8 @@
-import readlineSync from 'readline-sync';
+import readlineSync from "readline-sync";
 
 const askName = () => {
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
-}
+  const userName = readlineSync.question("May I have your name? ");
+  console.log("Hi " + userName + "!");
+};
 
 export default askName;
-
-
-
-
