393149774a87ff35474a32111f3abf0d49a20ba6
redkin <ilya.redking@gmail.com>
2020-09-21 22:53:59 -0400

    step 7 - done

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 <a href="https://asciinema.org/a/e9E1pU4ykDHXzxRFP4JxmeD4J">Demonstration of brain-even</a>
 
 <a href="https://asciinema.org/a/4w6UKwrpK2LISVtucYP7f0mRO">Demonstration of brain-calc</a>
+
+<a href="https://asciinema.org/a/jYNuhu00UGI0TqaBX6hh9BKGf">Demonstration of brain-calc</a>
--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -4,39 +4,37 @@ import _ from "lodash";
 import {userName} from "../src/cli.js";
 
 const checkGcd = () => {
-    console.log('Find the greatest common divisor of given numbers.');
-    for (let i = 0; i <= 2;) {
-        let a = _.random(1, 10),
-        b = _.random(1, 10);
-        let answer = readlineSync.question(`Question: ${a} ${b}\nYour answer:`);
-        let result;
-        if (a == b) {
-            result = a;
-          }
-          let min = Math.min(a, b),
-            max = Math.max(a, b);
-          for (let j = min; j > 0; j--) {
-            if (max % j === 0 && min % j === 0) {
-              result = j;
-              if (answer == result) { console.log("Correct!");
-            i++} else {
-                console.log(`"${answer}" is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
-                break;
-            }
-            }
-          }
-        
-        // if (answer == result) { console.log("Correct!");
-        //     i++} else {
-        //         console.log(`"${answer}" is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`);
-        //         break;
-        //     }
-            if (i===3) {
-                    console.log(`Congratulations, ${userName}!`);
-                    break;
-            }
-}
+  console.log("Find the greatest common divisor of given numbers.");
+  for (let i = 0; i <= 2; ) {
+    let a = _.random(1, 10),
+      b = _.random(1, 10);
+    let min = Math.min(a, b),
+      max = Math.max(a, b);
+    let answer = readlineSync.question(`Question: ${a} ${b}\nYour answer:`);
+    let result;
+
+    for (let j = min; j > 0; j--) {
+      if (max % j === 0 && min % j === 0) {
+        result = j;
+        break;
+      }
+    }
+       if (answer == result) {
+      console.log("Correct!");
+      i++;
+    } else {
+      console.log(
+        `"${answer}" is wrong answer ;(. Correct answer was ${result}.\nLet's try again, ${userName}!`
+      );
+      break;
+    }
+    if (i === 3) {
+      console.log(`Congratulations, ${userName}!`);
+    }
+  }
+  
 };
+
 
 checkGcd();
 export default checkGcd;
