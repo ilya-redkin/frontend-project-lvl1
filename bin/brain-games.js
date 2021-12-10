3c869ef8a078342f14fb7b96ee7b0dff2a934838
Ilya Redkin <ilya.redking@gmail.com>
2021-12-10 10:42:59 -0500

    new check 4

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import greeting from '../src/greeting.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
+greeting();
